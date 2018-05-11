4d33486650d37c6476bf0262e452c855d30d3090
CyRIC <35691890+CyRIC21@users.noreply.github.com>
2018-05-10 20:48:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+client.on("ready", () => {
+  console.log(`
+#####{ Bot Ready! }#####
+Users: ${client.user.tag}
+Bot Id: ${client.user.id}
+Servers: ${client.guilds.size}
+Users: ${client.users.size}
+########################`.trim());
+  console.log("Diamond is in: " + client.guilds.size + " servers.");
+  client.user.setActivity(`!! - In ${client.guilds.size} guilds!`);
+});
