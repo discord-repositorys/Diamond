2c85523e5e8a3109dfcd8d2569566e3bfe10be71
BloodyPikachu <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-14 00:31:11 -0500

    fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ Bot Id: ${client.user.id}
 Servers: ${client.guilds.size}
 Users: ${client.users.size}
 ########################`.trim());
-  console.log("Cryptide is in: " + client.guilds.size + " servers.");
+  console.log("Diamond is in: " + client.guilds.size + " servers.");
   client.user.setActivity(`d.help - In ${client.guilds.size} guilds!`);
 });
 
