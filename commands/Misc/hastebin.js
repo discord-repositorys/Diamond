01ee08075dddc455bfa128193911103f9ad1887f
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-05-15 17:16:26 +0300

    u kno this exist?

--- commands/Misc/hastebin.js
+++ commands/Misc/hastebin.js
@@ -1,4 +1,3 @@
-const hastebin = require('hastebin-gen');
 const Discord = require('discord.js');
 const { RichEmbed } = require("discord.js");
 
@@ -7,25 +6,22 @@ class Hastebin extends Command {
     super(client, {
       name: "hastebin",
       description: "Make a file in Hastebin.",
-      usage: "hastebin [code]",
+      usage: "hastebin <code>",
       catagory: "Misc",
       extended: "Make an easy to use hastebin file.",
       aliases: ["hbin", "haste", "bin"],
     });
   }
   
-  run(message, args) {
-    hastebin(args.join(' '), "js").then(r => {
-      var hastLink = r
-      const hastEmb = new Discord.RichEmbed()
-        .setTitle("Your hastebin link.")
-        .setURL(hastLink)
-        .setDescription("Click the link above, or click here.")
-        .setURL(hastLink)
-        .setTimestamp()
-        .setFooter(`Hastbin made by: ${msg.author.tag}`);
-      message.channel.send({embed: hastEmb});
-  });
+  async run(message, args) {
+    const h = await this.client.hastebin(args.join(" "), "js");
+    const hastEmb = new RichEmbed()
+      .setTitle("Your hastebin link.")
+      .setDescription(h)
+      .setTimestamp()
+      .setFooter(`Hastbin made by: ${message.author.tag}`);
+    message.channel.send({ embed: hastEmb });
+  }
 }
 
 module.exports = Hastebin;
