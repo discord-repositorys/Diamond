ef2b1da24eb698ebcefd05ad172cc854bf32e912
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-18 14:01:12 -0500

    fix play loli

--- commands/music/play.js
+++ commands/music/play.js
@@ -8,44 +8,24 @@ class Play extends Command {
       name: "play",
       description: "Play a song!",
       usage: "play [song]",
-      category: "Music",
+      catagory: "Music",
       extended: "Play a song from YouTube.",
       aliases: ["p"]
     });
   }
-  
-  run(message, args) {
-    const chan = message.member.voiceChannel;
-    if(!chan) return message.reply("You need to be in a voice channel!");
-    const song = args.join(" ");
-    if(!song) return message.reply("Provide a youtube link to play or search!");
-    chan.join().then(async con => {
-      if (song.match(/https:\/\/?www\.?youtube\.com\/watch\?v=(.*)/)) {
-        try {
-          const video = this.client.music.fromURL(song[2]);
-          con.play(ytdl(video.url, { filter: "audioonly" }));
-          message.channel.send(`Now playing from url: **${video.title}**`); 
-        } catch (error) {
-          return message.channel.send(`\`\`\`js\n${error.stack}\`\`\``);
-        }
-      } else if (song.match(/https:\/\/?(www\.)?youtu\.be\/(.*)/)) {
-        try {
-          const video = this.client.music.fromURL(song[2]);
-          con.play(ytdl(video.url, { filter: "audioonly" }));
-          message.channel.send(`Now playing from url: **${video.title}**`); 
-        } catch (error) {
-          return message.channel.send(`\`\`\`js\n${error.stack}\`\`\``);
-        }
-      } else {
-        try {
-          const videos = this.client.music.fromSearch(song);
-          con.play(ytdl(videos[0].url, { filter: "audioonly" }));
-          message.channel.send(`Now playing from search: **${videos[0].title}**`); 
-        } catch (error) {
-          return message.channel.send(`\`\`\`js\n${error.stack}\`\`\``);
-        }
-      }
-    });
-  }
+      
+
+run(message, args) {
+  const chan = message.member.voiceChannel;
+  if(!chan) return message.reply("You need to be in a voice channel!");
+  const song = args.join(" ");
+  if(!song) return message.reply("Provide a youtube link to play!");
+chan.join()
+  .then(connection => {
+  const dispatcher = connection.playStream(ytdl(song, { audioonly: true }));
+  dispatcher.on("end", () => chan.leave());
+}).catch(console.error);
 }
+}
+  
 module.exports = Play;
