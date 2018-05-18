b2c7a5fdd5d34c8d5ec9b6b536f06c21bd73d9ac
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-18 18:35:50 -0500

    maybe fix

--- commands/music/play.js
+++ commands/music/play.js
@@ -19,37 +19,47 @@ class Play extends Command {
       
 
 run(message, args) {
-  if (!message.member.voiceChannel) return message.reply('Connect to a voice channel first.');
-            if (guild.isPlaying) {
-               getID(song, id => {
-                  if (!id) return message.reply('Unable to extract video.');
-                  ytdl.getInfo(id, (err, info) => {
-                     if (err) return message.reply('Unable to extract video');
-                     if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
-                     message.delete();
-                        guild.queue.push({
-                           info, requester: message.member
+  let guild = music[message.guild.id];
+        if (!guild) guild = music[message.guild.id] = {
+            queue: [],
+            skippers: [],
+            skipReq: 0,
+            isPlaying: false
+    };
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+        let song = args.join(' ')
+        if (!message.member.voiceChannel) return message.reply('Connect to a voice channel first.');
+                  if (guild.isPlaying) {
+                     getID(song, id => {
+                        if (!id) return message.reply('Unable to extract video.');
+                        ytdl.getInfo(id, (err, info) => {
+                           if (err) return message.reply('Unable to extract video');
+                           if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
+                           message.delete();
+                              guild.queue.push({
+                                 info, requester: message.member
+                           });
+                           message.reply(`The song: **${info.title}** has been added to the queue list.`);
+
+                        });
                      });
-                     message.reply(`The song: **${info.title}** has been added to the queue list.`);
-            
-                  });
-               });
-            
-            } else {
-              guild.isPlaying = true;
-               getID(song, id => {
-               if (!id) return message.reply(' unable to extract video');
-                  ytdl.getInfo(id, (err, info) => {
-                  if (err) return message.reply('Hmm..there was an error extracting that video.');
-                  if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
-                     message.delete();
-                          guild.queue.push({
-                           info, requester: message.member
+
+                  } else {
+                    guild.isPlaying = true;
+                     getID(song, id => {
+                     if (!id) return message.reply(' unable to extract video');
+                        ytdl.getInfo(id, (err, info) => {
+                        if (err) return message.reply('Hmm..there was an error extracting that video.');
+                        if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
+                           message.delete();
+                                guild.queue.push({
+                                 info, requester: message.member
+                           });
+                           playMusic(guild, message);
+                        });
                      });
-                     playMusic(guild, message);
-                  });
-               });
-             }
+                   }
 }
 }
   
