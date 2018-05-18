6ae8efe60a5fad37e1437d7c613b57554419a268
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-18 17:54:12 -0500

    maybe

--- commands/music/play.js
+++ commands/music/play.js
@@ -16,16 +16,37 @@ class Play extends Command {
       
 
 run(message, args) {
-  const chan = message.member.voiceChannel;
-  if(!chan) return message.reply("You need to be in a voice channel!");
-  const song = args.join(" ");
-  if(!song) return message.reply("Provide a youtube link to play!");
-chan.join()
-  .then(connection => {
-  const dispatcher = connection.playStream(ytdl(song, { audioonly: true }));
-  message.channel.send(`Now Playing: ${song}`);
-  dispatcher.on("end", () => chan.leave());
-}).catch(console.error);
+  if (!message.member.voiceChannel) return message.reply('Connect to a voice channel first.');
+            if (guild.isPlaying) {
+               getID(song, id => {
+                  if (!id) return message.reply('Unable to extract video.');
+                  ytdl.getInfo(id, (err, info) => {
+                     if (err) return message.reply('Unable to extract video');
+                     if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
+                     message.delete();
+                        guild.queue.push({
+                           info, requester: message.member
+                     });
+                     message.reply(`The song: **${info.title}** has been added to the queue list.`);
+            
+                  });
+               });
+            
+            } else {
+              guild.isPlaying = true;
+               getID(song, id => {
+               if (!id) return message.reply(' unable to extract video');
+                  ytdl.getInfo(id, (err, info) => {
+                  if (err) return message.reply('Hmm..there was an error extracting that video.');
+                  if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
+                     message.delete();
+                          guild.queue.push({
+                           info, requester: message.member
+                     });
+                     playMusic(guild, message);
+                  });
+               });
+             }
 }
 }
   
