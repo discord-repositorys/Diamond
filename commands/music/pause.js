c334a88af1df5ed45af53e000fb27695b4b2133d
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-19 09:21:34 -0500

    Create pause.js

---
+++ commands/music/pause.js
@@ -0,0 +1,4 @@
+const Command = require("../../base/Command.js");
+const { RichEmbed } = require("discord.js");
+const { play, search_video, playMusic, skip_song, getID } = require("../../utils/music.js")
+const ytdl = require("ytdl-core");
