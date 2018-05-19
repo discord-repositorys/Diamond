27202b39f6fb08ab4d42bf709e2e64907783d6db
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-19 10:10:19 -0500

    Create queue.js

---
+++ commands/music/queue.js
@@ -0,0 +1,23 @@
+
+const Command = require("../../base/Command.js");
+const { RichEmbed } = require("discord.js");
+const { play, search_video, playMusic, skip_song, getID } = require("../../utils/music.js")
+const ytdl = require("ytdl-core");
+
+class Queue extends Command {
+  constructor(client) {
+    super(client, {
+      name: "queue",
+      description: "Show the current queue.",
+      usage: "queue",
+      category: "Music",
+      extended: "Show the music queue. You do not have to be in a music channel for this command to work."
+    });
+}
+
+run(message, args) {
+  if (!guild) return message.reply('No songs in queue.');
+            message.channel.send(`\`\`\`Queue:\n${guild.queue.map(a => `**${a.info.title}** as requested by **${a.requester.user.username}**`).join('\n\n') || 'No songs currently queued!'}\`\`\``)
+}
+}
+module.exports = Queue;
