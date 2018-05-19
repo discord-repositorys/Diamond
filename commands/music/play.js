98fdc2ed7c2ecf0eb04c1a3732321ce052b49eeb
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-19 10:45:01 -0500

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -27,37 +27,37 @@ run(message, args) {
     };
         const command = args.shift().toLowerCase();
         let song = args.join(' ')
-        if (!message.member.voiceChannel) return message.reply('Connect to a voice channel first.');
-                  if (guild.isPlaying) {
-                     getID(song, id => {
-                        if (!id) return message.reply('Unable to extract video.');
-                        ytdl.getInfo(id, (err, info) => {
-                           if (err) return message.reply('Unable to extract video');
-                           if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
-                           message.delete();
-                              guild.queue.push({
-                                 info, requester: message.member
-                           });
-                           message.reply(`The song: **${info.title}** has been added to the queue list.`);
-
-                        });
+        if (!message.member.voiceChannel) return message.reply('Err...No voicechannel?');
+            if (guild.isPlaying) {
+               getID(song, id => {
+                  if (!id) return message.reply('Unable to extract video.');
+                  ytdl.getInfo(id, (err, info) => {
+                     if (err) return message.reply('Hmm..there was an error extracting that video.');
+                     if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
+                     message.delete();
+                        guild.queue.push({
+                           info, requester: message.member
                      });
-
-                  } else {
-                    guild.isPlaying = true;
-                     getID(song, id => {
-                     if (!id) return message.reply(' unable to extract video');
-                        ytdl.getInfo(id, (err, info) => {
-                        if (err) return message.reply('Hmm..there was an error extracting that video.');
-                        if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
-                           message.delete();
-                                guild.queue.push({
-                                 info, requester: message.member
-                           });
-                           playMusic(guild, message);
-                        });
+                     message.reply(`The song: **${info.title}** has been added to the queue list.`);
+            
+                  });
+               });
+            
+            } else {
+              guild.isPlaying = true;
+               getID(song, id => {
+               if (!id) return message.reply(' unable to extract video');
+                  ytdl.getInfo(id, (err, info) => {
+                  if (err) return message.reply('Hmm..there was an error extracting that video.');
+                  if (info.formats.some(format => format.live)) return message.reply('Not supporting live stream at this time.');
+                     message.delete();
+                          guild.queue.push({
+                           info, requester: message.member
                      });
-                   }
+                     playMusic(guild, message);
+                  });
+               });
+             }
 }
 }
   
