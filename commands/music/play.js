a5a8e9c40cddc524327a9500cc3941d94acf892f
CyRIC <35691890+CyRIC21@users.noreply.github.com>
2018-05-18 21:42:42 -0400

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,7 +1,7 @@
 const Command = require("../../base/Command.js");
 const { RichEmbed } = require("discord.js");
 const { play, search_video, playMusic, skip_song, getID } = require("../../utils/music.js")
-const ytdl = require("ytdl");
+const ytdl = require("ytdl-core");
 
 class Play extends Command {
   constructor(client) {
