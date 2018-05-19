682b0cc5a869c0b6ea94b4d788137acea1e9c112
PIKAAAAAAAAACHUUUUUUUUUUU <36680137+BloodyPikachu@users.noreply.github.com>
2018-05-19 09:44:29 -0500

    pause shud werk

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -12,3 +12,19 @@ class Resume extends Command {
       extended: "Resume a song that Diamond is playing. Must be in the voice channel in order for this to work.",
     });
   }
+  
+  run(message, args) {
+    let voiceChannel = message.member.voiceChannel
+            if (!voiceChannel || voiceChannel.id !== message.guild.voiceConnection.channel.id) return message.reply(`You must be in a voice channel to pause the music.`);
+            try {
+                message.guild.voiceConnection.dispatcher.resume();
+               return message.reply('Successfully resumed playing.')
+            } catch (err) {
+                console.error(err)
+                return message.reply('Sorry, there was an error resuming that song.')
+            }
+}
+}
+
+
+module.exports = Resume;
